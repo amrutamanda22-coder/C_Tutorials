319127bd32f645e1433ca205d5ee6dcf53350879
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-11-08 00:00:51 +0530

    Different types of Arithmetic Operators

---
+++ Program6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    // arithmetic operators = + - * / % ++ --
+
+    int x = 10;
+    int y = 2;
+    int z = 0;
+
+    //z = x + y;
+    //z = x - y;
+    //z = x * y;
+    //z = x / y;
+    //z = x % y;
+    //z = x % 2;
+
+    //augumented assignment operators
+    //x+=3;
+    //x-=3;
+    //x*=2;
+    //x/=2;
+
+
+    printf("%d",x);
+
+    return 0;
+}
