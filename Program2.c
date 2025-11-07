c479f4231f68653cafa1061aea3edd3224082ec0
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-11-07 23:55:45 +0530

    2nd and 3rd Basic Programs

---
+++ Program2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    //This is my second c program 
+
+    /*This 
+    is 
+    also 
+    a 
+    comment
+    */
+
+    printf("I like burgers! \n");
+    printf("It's really good!");
+    
+    return 0;
+
+}
---
+++ Program3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    // This program prints my name
+
+    /*This
+    is 
+    third
+    program
+    */
+
+    printf("My name is :");
+    printf("Manda Amruta");
+
+    return 0;
+}
