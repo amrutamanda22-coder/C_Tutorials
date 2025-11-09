333cffa72bfa9370506dfadf44fddf6c93c3bde7
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-11-09 23:50:37 +0530

    Math Functions

---
+++ Program10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main(){
+    float x = 45;
+
+    //x = sqrt(x);
+    //x = pow(x,3);
+    //x = round(x);
+    //x = ceil(x); //round up
+    //x = floor(x); //round down
+    //x = abs(x); //turn neg no to pos no
+    //x = log(x);
+    //x = sin(x);
+    //x = cos(x);
+    //x = tan(x);
+
+
+    printf("%f",x);
+
+    return 0;
+}
